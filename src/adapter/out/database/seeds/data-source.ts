30be4dc19d08c8c9b41fa096e42d2f53b153f2f6
jtrujillo1 <johnelias1.developer@gmail.com>
2025-04-27 16:02:35 -0500

    fix: host

--- src/adapter/out/database/seeds/data-source.ts
+++ src/adapter/out/database/seeds/data-source.ts
@@ -10,7 +10,7 @@ import {
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
